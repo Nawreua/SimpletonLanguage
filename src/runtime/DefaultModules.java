67b249f0bdca38bf0cf0093f20099d95d930ddf1
Erwan Auer <erwan.auer@epita.fr>
2019-10-24 23:11:55 +0200

    runtime: add new runtime builtins
    Signed-off-by: Erwan Auer <erwan.auer@epita.fr>

--- src/runtime/DefaultModules.java
+++ src/runtime/DefaultModules.java
@@ -3,7 +3,7 @@ package runtime;
 import java.util.Map;
 
 public class DefaultModules {
-    private static String operators =
+    public static String operators =
             "primitive add being int takes l being int and r being int end\n" +
                     "primitive sub being int takes l being int and r being int end\n" +
                     "primitive div being int takes l being int and r being int end\n" +
@@ -12,16 +12,16 @@ public class DefaultModules {
                     "primitive not being boolean takes b being boolean end\n" +
                     "primitive land being boolean takes l being boolean and r being boolean end\n" +
                     "primitive lor being boolean takes l being boolean and r being boolean end\n" +
-                    "primitive eq being boolean takes l being int and r being int end";
+                    "primitive eq being boolean takes l being int and r being int end\n" +
+                    "primitive ne being boolean takes l being int and r being int end\n" +
+                    "primitive lt being boolean takes l being int and r being int end\n" +
+                    "primitive le being boolean takes l being int and r being int end\n" +
+                    "primitive gt being boolean takes l being int and r being int end\n" +
+                    "primitive ge being boolean takes l being int and r being int end";
 
-    private static String output =
+    public static String output =
             "primitive print being boolean takes i being int end\n" +
                     "primitive lprint being boolean takes b being boolean end\n" +
                     "primitive println being boolean takes i being int end\n" +
                     "primitive lprintln being boolean takes b being boolean end";
-
-    public static Map<String, String> modules = Map.of(
-            "operators", operators,
-            "output", output
-    );
 }
--- src/runtime/RuntimeFunctions.java
+++ src/runtime/RuntimeFunctions.java
@@ -1,41 +1,124 @@
 package runtime;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import java.util.Stack;
 
 public class RuntimeFunctions {
 
+    private static Stack<Object> arguments = new Stack<>();
+
     public static Object dispatchPrimitive(String primitive, List<Object> values) {
-        if (primitive.equals("println"))
-            return println((Integer) values.get(0));
-        if (primitive.equals("not"))
-            return not((Boolean)values.get(0));
-        if (primitive.equals("add"))
-            return add((Integer) values.get(0), (Integer)values.get(1));
-        if (primitive.equals("eq"))
-            return eq((Integer)values.get(0), (Integer)values.get(1));
-        if (primitive.equals("mul"))
-            return mul((Integer)values.get(0), (Integer)values.get(1));
-        if (primitive.equals("sub"))
-            return sub((Integer)values.get(0), (Integer)values.get(1));
-        return null;
+        values.forEach(arguments::push);
+        Object value = null;
+        try {
+            value = RuntimeFunctions.class.getDeclaredMethod(primitive).invoke(null);
+        } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < values.size(); i++) arguments.pop();
+        return value;
     }
 
-    private static boolean println(int i) {
-        System.out.println(i);
+    private static boolean print() {
+        System.out.print(arguments.peek());
         return true;
     }
 
-    private static boolean not(boolean b) {
-        return !b;
+    private static boolean println() {
+        System.out.println(arguments.peek());
+        return true;
     }
 
-    private static int add(int l, int r) {
+    private static boolean lprint() {
+        System.out.print(arguments.peek());
+        return true;
+    }
+
+    private static boolean lprintln() {
+        System.out.println(arguments.peek());
+        return true;
+    }
+
+    private static boolean not() {
+        return !(Boolean)arguments.peek();
+    }
+
+    private static int add() {
+        var r = (Integer)arguments.get(arguments.size() - 1);
+        var l = (Integer)arguments.get(arguments.size() - 2);
         return l + r;
     }
 
-    private static int sub(int l, int r) { return l - r; }
+    private static int sub() {
+        var r = (Integer)arguments.get(arguments.size() - 1);
+        var l = (Integer)arguments.get(arguments.size() - 2);
+        return l - r;
+    }
 
-    private static boolean eq(int l, int r) { return  l == r; }
+    private static boolean land() {
+        var r = (Boolean)arguments.get(arguments.size() - 1);
+        var l = (Boolean)arguments.get(arguments.size() - 2);
+        return l && r;
+    }
 
-    private static int mul(int l, int r) { return l * r; }
+    private static boolean lor() {
+        var r = (Boolean)arguments.get(arguments.size() - 1);
+        var l = (Boolean)arguments.get(arguments.size() - 2);
+        return l || r;
+    }
+
+    private static boolean eq() {
+        var r = (Integer)arguments.get(arguments.size() - 1);
+        var l = (Integer)arguments.get(arguments.size() - 2);
+        return l.equals(r);
+    }
+
+    private static int mul() {
+        var r = (Integer)arguments.get(arguments.size() - 1);
+        var l = (Integer)arguments.get(arguments.size() - 2);
+        return l * r;
+    }
+
+    private static int div() {
+        var r = (Integer)arguments.get(arguments.size() - 1);
+        var l = (Integer)arguments.get(arguments.size() - 2);
+        return l / r;
+    }
+
+    private static int mod() {
+        var r = (Integer)arguments.get(arguments.size() - 1);
+        var l = (Integer)arguments.get(arguments.size() - 2);
+        return l % r;
+    }
+
+    private static boolean ne() {
+        var r = (Integer)arguments.get(arguments.size() - 1);
+        var l = (Integer)arguments.get(arguments.size() - 2);
+        return !l.equals(r);
+    }
+
+    private static boolean lt() {
+        var r = (Integer)arguments.get(arguments.size() - 1);
+        var l = (Integer)arguments.get(arguments.size() - 2);
+        return l < r;
+    }
+
+    private static boolean le() {
+        var r = (Integer)arguments.get(arguments.size() - 1);
+        var l = (Integer)arguments.get(arguments.size() - 2);
+        return l <= r;
+    }
+
+    private static boolean gt() {
+        var r = (Integer)arguments.get(arguments.size() - 1);
+        var l = (Integer)arguments.get(arguments.size() - 2);
+        return l > r;
+    }
+
+    private static boolean ge() {
+        var r = (Integer)arguments.get(arguments.size() - 1);
+        var l = (Integer)arguments.get(arguments.size() - 2);
+        return l >= r;
+    }
 }
